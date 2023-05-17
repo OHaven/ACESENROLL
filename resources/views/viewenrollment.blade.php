338923c95902d2d43585adb119dc7b8235c8bb55
OHaven <gyumeen14321@gmail.com>
2023-05-18 07:39:02 +0800

    Update viewenrollment.blade.php

--- resources/views/viewenrollment.blade.php
+++ resources/views/viewenrollment.blade.php
@@ -7,8 +7,35 @@
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-xl sm:rounded-lg">
-            
+            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-xl sm:rounded-lg" style="padding: 2em;">
+
+                @if($pfp[0] == null)
+                <div class="mt-2" x-show="photoPreview" style="margin: 5em; margin-left: 45%;">
+                    <span class="block rounded-full w-20 h-20 bg-cover bg-no-repeat bg-center"
+                          x-bind:style="'background-image: url(\'' + photoPreview + '\');'">
+                          No Profile Photo Set
+                    </span>
+                </div>
+                @else
+                <div class="mt-2" x-show="! photoPreview" style="margin: 5em; margin-left: 40%;">
+                    <img style="height: 15em; width: 15em;" src="/storage/{{ $pfp[0] }}" alt="{{ $name[0] }}" class="rounded-full h-20 w-20 object-cover">
+                </div>
+                @endif
+
+
+                <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight" style="margin-left: 2em;">
+                    {{ __('Name: ') }} {{$name[0]}}
+                </h2>
+                    <br>
+                <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight" style="margin-left: 2em;">
+                    {{ __('Email: ') }} {{$email[0]}}
+                </h2>
+
+                <br>
+                <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight" style="margin-left: 2em;">
+                    {{ __('Role: ') }} {{$role[0]}}
+                </h2>
+
             </div>
         </div>
     </div>
