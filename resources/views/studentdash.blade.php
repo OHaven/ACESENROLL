c6b40747f0aa24cbd7f0a273f54da917df4184e2
OHaven <gyumeen14321@gmail.com>
2023-05-18 04:27:52 +0800

    V5

--- resources/views/studentdash.blade.php
+++ resources/views/studentdash.blade.php
@@ -7,8 +7,19 @@
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-xl sm:rounded-lg">
-                
+            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-xl sm:rounded-lg" style="padding:2em;">
+            <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+            {{ __('Upload Clearance') }}
+        </h2>     
+        <form style="margin: 1em;" method="POST" enctype="multipart/form-data">
+        @csrf
+  <label for="file-input" class="sr-only">Choose file</label>
+  <input type="file" name="file-input" id="file-input" class="block w-full border border-gray-200 shadow-sm rounded-md text-sm focus:z-10 focus:border-blue-500 focus:ring-blue-500 dark:bg-slate-900 dark:border-gray-700 dark:text-gray-400
+    file:bg-transparent file:border-0
+    file:bg-gray-100 file:mr-4
+    file:py-3 file:px-4
+    dark:file:bg-gray-700 dark:file:text-gray-400">
+</form>
             </div>
         </div>
     </div>
